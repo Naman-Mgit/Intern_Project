17cfe5922d82d5f5ad5ba9937ffbd11a0ddbc689
Naman <naman.apk.com@gmail.com>
2025-06-02 00:06:04 +0530

    sc

--- lms-frontend/src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import globalReducer from './globalSlice';
-
-const rootReducer = combineReducers({
-    global: globalReducer,
-});
-
-export default rootReducer;
